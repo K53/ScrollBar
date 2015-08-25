9b772f9390438f3bf00f10a57153ad18252712d3
furukawa keigo <usirono-keputasanta24@hotmail.co.jp>
2015-08-25 14:28:06 -0700

    first commit

---
+++ scroll_bar/index.coffee
@@ -0,0 +1,51 @@
+﻿# CoffeeScript
+$ () ->
+    instance = new ScrollBar(".content", ".scroll", ".bar", ".bar-rail", "bar-rail-active")
+        
+class window.ScrollBar
+     constructor: (content, scroll, bar, barRail, barRailActive) ->
+        @content         =   content
+        @scroll          =   scroll
+        @bar             =   bar
+        @barRail         =   barRail
+        @barRailActive   =   barRailActive
+        @delay = 1500
+        @timer = 0
+        
+        $(@scroll).on "scroll", @scrollStopEventTrigger.bind this 
+
+        $(@scroll).on "scroll", @barSize.bind this 
+
+        $(window).on "resize load", @barSize.bind this 
+
+        $(@barRail).on "mouseenter", @showBar.bind this
+        
+        $(window).on "scrollstop mouseleave", () =>
+            $(@barRail).removeClass @barRailActive
+            
+     barSize : -> 
+        totalHeight     =   $(@content).outerHeight()
+        visibleHeight   =   $(window).height()
+        barHeight       =   visibleHeight / totalHeight * visibleHeight
+        top             =   $(@scroll).scrollTop() * visibleHeight / totalHeight
+
+        $(@bar).css "height",barHeight
+        $(@bar).css "top", top
+
+        @showBar()
+        undefined
+    
+     showBar : -> 
+        $(@barRail).addClass @barRailActive
+        undefined
+
+     scrollStopEventTrigger : -> 
+        if @timer
+            clearTimeout @timer
+        @timer = setTimeout(() => 
+            $(window).trigger "scrollstop"
+            undefined
+        , @delay)
+        undefined
+
+
---
+++ scroll_bar/index.js
@@ -0,0 +1,63 @@
+﻿(function() {
+  $(function() {
+    var instance;
+    return instance = new ScrollBar(".content", ".scroll", ".bar", ".bar-rail", "bar-rail-active");
+  });
+
+  window.ScrollBar = (function() {
+    function ScrollBar(content, scroll, bar, barRail, barRailActive) {
+      this.content = content;
+      this.scroll = scroll;
+      this.bar = bar;
+      this.barRail = barRail;
+      this.barRailActive = barRailActive;
+      this.delay = 1500;
+      this.timer = 0;
+      $(this.scroll).on("scroll", this.scrollStopEventTrigger.bind(this));
+      $(this.scroll).on("scroll", this.barSize.bind(this));
+      $(window).on("resize load", this.barSize.bind(this));
+      $(this.barRail).on("mouseenter", this.showBar.bind(this));
+      $(window).on("scrollstop mouseleave", (function(_this) {
+        return function() {
+          return $(_this.barRail).removeClass(_this.barRailActive);
+        };
+      })(this));
+    }
+
+    ScrollBar.prototype.barSize = function() {
+      var barHeight, top, totalHeight, visibleHeight;
+      totalHeight = $(this.content).outerHeight();
+      visibleHeight = $(window).height();
+      barHeight = visibleHeight / totalHeight * visibleHeight;
+      top = $(this.scroll).scrollTop() * visibleHeight / totalHeight;
+      $(this.bar).css("height", barHeight);
+      $(this.bar).css("top", top);
+      this.showBar();
+      return void 0;
+    };
+
+    ScrollBar.prototype.showBar = function() {
+      $(this.barRail).addClass(this.barRailActive);
+      return void 0;
+    };
+
+    ScrollBar.prototype.scrollStopEventTrigger = function() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      this.timer = setTimeout((function(_this) {
+        return function() {
+          $(window).trigger("scrollstop");
+          return void 0;
+        };
+      })(this), this.delay);
+      return void 0;
+    };
+
+    return ScrollBar;
+
+  })();
+
+}).call(this);
+
+//# sourceMappingURL=index.js.map
